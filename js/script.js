dafc550aa662404007b328bd58830ace8fa9bd11
Martin Wong <martin.wong@amethodschools.org>
2018-10-25 01:00:43 +0000

    added new variables and click handlers

--- js/script.js
+++ js/script.js
@@ -9,6 +9,25 @@
 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
-    $("#userChoice").text($("#input").val());
+     userChoice = $("#input").val()
+    $("#userChoice").text(userChoice);
+     randomNumber = Math.random();
+     if( randomNumber > .66666) {
+        // var computerChoice="rock"
+         $("#computerChoice").html("rock"); 
+    }else if(randomNumber < .66666 && randomNumber > .33333) {
+    //   var computerChoice="paper"
+        $("#computerChoice").html("paper");
+    } else {
+    //   var computerChoice="scissors"
+         $("#computerChoice").html("scissor"); 
+    }
 });
 // $("#computerChoice").text($("#").val());
+ var userChoice = "";
+ var computerChoice = "";
+ var winner = "";
+ var randomNumber = Math.random();
+  
+ 
+ 
